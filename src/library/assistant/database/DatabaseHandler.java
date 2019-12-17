f6bb0d39b2ebb1179997dca8a833b883be98fc75
Sistemas <Sistemas@Sistemas>
2019-12-17 14:50:34 -0500

    Creando tabla para ver libros

--- src/library/assistant/database/DatabaseHandler.java
+++ src/library/assistant/database/DatabaseHandler.java
@@ -2,10 +2,15 @@ package library.assistant.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 public class DatabaseHandler {
     
+    
+    private Statement statement;
     private Connection connection;
     //private String url = "jdbc:h2:file:./src/BaseH2/libreria_db";
     private String url = "jdbc:mysql://bujgjsuf73do1mmlrclk-mysql.services.clever-cloud.com:3306/bujgjsuf73do1mmlrclk?autoReconnect=true&useSSL=false";
@@ -40,5 +45,30 @@ public class DatabaseHandler {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    
+    public ResultSet excecQuery(String query){
+        ResultSet result;
+        try {
+            statement = connection.createStatement();
+            result = statement.executeQuery(query);            
+        } catch (SQLException e) {
+            System.out.println("Error en excecQuery: " + e.getLocalizedMessage());
+            return null;
+        }
+        return result;
+    }
+    
+    public boolean excecAction(String query){
+        try {
+            statement = connection.createStatement();
+            statement.executeQuery(query);
+            return true;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error:" + e.getMessage(), "Error ocurrido", JOptionPane.ERROR_MESSAGE);
+            System.out.println("Error en excecAction: " + e.getLocalizedMessage());
+            return false;
+        }
+        
     }
 }
--- src/library/assistant/ui/addbook/FXMLDocumentController.java
+++ src/library/assistant/ui/addbook/FXMLDocumentController.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package library.assistant.ui.addbook;
 
 import Modelo.Book;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import library.assistant.database.DatabaseHandler;
-
 
 /**
  *
@@ -35,15 +35,17 @@ public class FXMLDocumentController implements Initializable {
     private JFXButton saveButton;
     @FXML
     private JFXButton cancelButton;
-    
+
     DatabaseHandler databasehandler;
     
-    
-    
+    @FXML
+    private AnchorPane rootPane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         databasehandler = new DatabaseHandler();
-    }    
+        chekData();
+    }
 
     @FXML
     private void addBook(ActionEvent event) {
@@ -51,24 +53,44 @@ public class FXMLDocumentController implements Initializable {
         String bookAuthor = author.getText();
         String bookName = title.getText();
         String bookPublisher = publisher.getText();
-        
-        if(bookID.isEmpty()||bookAuthor.isEmpty()||bookName.isEmpty()||bookPublisher.isEmpty()){
+
+        if (bookID.isEmpty() || bookAuthor.isEmpty() || bookName.isEmpty() || bookPublisher.isEmpty()) {
             Alert msj = new Alert(Alert.AlertType.ERROR);
             msj.setHeaderText(null);
             msj.setContentText("Please Enter in all fields");
             msj.showAndWait();
             return;
-        }else{
+        } else {
             Book libro = new Book(bookID, bookName, bookAuthor, bookPublisher, true);
             databasehandler.establecerConexion();
-            libro.guardarRegistro(databasehandler.getConnection());
-            databasehandler.cerrarConexion();            
+            int resultado = libro.guardarRegistro(databasehandler.getConnection());
+            databasehandler.cerrarConexion();
+            if (resultado == 1) {
+                Alert msj = new Alert(Alert.AlertType.INFORMATION);
+                msj.setTitle("Registro agregado");
+                msj.setContentText("El registro ha sido agregado exitósamente");
+                msj.setHeaderText("Resultado");
+                msj.show();
+            }
         }
     }
 
     @FXML
     private void cancel(ActionEvent event) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
     }
-    
-        
+
+    private void chekData() {
+        String sql = "SELECT title FROM book";
+        ResultSet rs = databasehandler.excecQuery(sql);
+        try {
+            while (rs.next()) {
+                String title = rs.getString("title");
+                System.out.println(title);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
