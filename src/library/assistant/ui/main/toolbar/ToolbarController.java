431472bb26bf6a4c5d3a85b49ad51e3464b4e23a
Sistemas <Sistemas@Sistemas>
2020-01-13 16:47:05 -0500

    Tutorial 33 diseño en curso

---
+++ src/library/assistant/ui/main/toolbar/ToolbarController.java
@@ -0,0 +1,16 @@
+package library.assistant.ui.main.toolbar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class ToolbarController implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
